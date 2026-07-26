ae4ee8b27708306540d244984b31cd18eb5d02b4
gjolapamo3 <127635814+gjolapamo3@users.noreply.github.com>
2026-07-26 14:10:18 -0500

    Update src/routes/orderRoutes.js with protected buyer order route

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -2,8 +2,16 @@ const express = require('express');
 const router = express.Router();
 const { createOrder } = require('../controllers/orderController');
 const { validateOrderInput } = require('../middleware/validateOrder');
+const { verifyToken } = require('../middleware/auth');
+const { checkRole } = require('../middleware/checkRole');
 
-// POST /api/orders
-router.post('/', validateOrderInput, createOrder);
+// Protected Route: Must be logged in AND have role 'buyer'
+router.post(
+  '/', 
+  verifyToken, 
+  checkRole('buyer'), 
+  validateOrderInput, 
+  createOrder
+);
 
 module.exports = router;
